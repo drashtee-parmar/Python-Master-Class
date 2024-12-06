83632432d53dac14ed8a9fc30148b5d6fe1a5fbc
Drashtee Parmar <drashteeparmar@gmail.com>
2024-12-06 08:49:28 -0500

    adding and removing item from list to keep track

--- Sequences/buy_computer.py
+++ Sequences/buy_computer.py
@@ -1,5 +1,5 @@
 available_parts = ["computer",
-                    "monitor",
+                   "monitor",
                    "keyboard",
                    "mouse",
                    "hdmi cable",
@@ -8,19 +8,27 @@ available_parts = ["computer",
 
 # valid_choices = [str(i) for i in range (1, len(available_parts) + 1)] # comprehension
 valid_choices = []
-for i in range (1, len(available_parts) + 1):
+for i in range(1, len(available_parts) + 1):
     valid_choices.append(str(i))
 print(valid_choices)
 
 current_choice = "_"
-computer_parts = [] # create empty list of computer parts
+computer_parts = []  # create empty list of computer parts
 
 while current_choice != '0':
     if current_choice in valid_choices:
-        print("Adding {}".format(current_choice))
         index = int(current_choice) - 1
         chosen_part = available_parts[index]
-        computer_parts.append(chosen_part)
+        # check to see if the parts are available in stock
+        if chosen_part in computer_parts:
+            #     its already in, so remove it
+            print("Removing {}".format(current_choice))
+
+            computer_parts.remove(chosen_part)
+        else:
+            print("Adding {}".format(current_choice))
+            computer_parts.append(chosen_part)
+        print("Your list now contains {}".format(computer_parts))
         # if current_choice == '1':
         #     computer_parts.append("computer")
         # elif current_choice == '2':
@@ -38,7 +46,7 @@ while current_choice != '0':
     else:
         print("Please add options from the list below:")
         for number, part in enumerate(available_parts):
-            print("{0}: {1}".format(number + 1, part)) # number : index position
+            print("{0}: {1}".format(number + 1, part))  # number : index position
 
     current_choice = input()
 
