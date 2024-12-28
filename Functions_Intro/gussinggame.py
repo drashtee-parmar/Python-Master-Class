807655f87eb02ab3505f38eb002ea530409ba201
Drashtee Parmar <drashteeparmar@gmail.com>
2024-12-28 17:38:57 -0500

    gussing game

---
+++ Functions_Intro/gussinggame.py
@@ -0,0 +1,37 @@
+import random
+
+
+def get_integer(prompt):
+    while True:
+        temp = input(prompt)
+        if temp.isnumeric():
+            return int(temp)
+        # else:
+        #     print("{0} is not a valid number". format(temp))
+        print("{0} is not a valid number". format(temp))
+
+
+highest = 1000
+answer = random.randint(1, highest)
+print(answer)
+guess = 0  # initialise to any number that does not equal to answer
+print("Please guess the number between 1 and {}: ".format(highest))
+
+while guess != answer:
+    guess = get_integer(": ")
+
+    if guess == 0:
+        break
+    if guess == answer:
+        print("Well done, you guessed it")
+        break
+    else:
+        if guess < answer:
+            print("Please guess higher")
+        else:
+            print("Please guess lower")
+        # guess = int(input())
+        # if guess == answer:
+        #     print("Well done, you guessed it")
+        # else:
+        #     print("Sorry, you have not guessed correctly.")
