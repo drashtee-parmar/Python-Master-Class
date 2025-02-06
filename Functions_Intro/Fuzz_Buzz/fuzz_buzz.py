b35b2eaed234749a25a232f6a346f9a6a70b5715
Drashtee Parmar <drashteeparmar@gmail.com>
2025-02-05 19:04:25 -0500

    fizz buzz test

--- Functions_Intro/Fuzz_Buzz/fuzz_buzz.py
+++ Functions_Intro/Fuzz_Buzz/fuzz_buzz.py
@@ -18,5 +18,23 @@ def fizz_buzz(num: int) -> str:
         return str(num)
 
 
-for i in range(1, 101):
-    print(fizz_buzz(i))
+# for i in range(1, 101):
+#     print(fizz_buzz(i))
+
+input("Play Fizz Buzz.  Press Enter to start")
+print()
+
+next_number = 0
+while next_number < 99:
+    next_number += 1
+    print(fizz_buzz(next_number))
+    next_number += 1
+    correct_answer = fizz_buzz(next_number)
+    players_answer = input("Your go: ")
+    # players_answer = correct_answer
+    if players_answer != correct_answer:
+        print("You lose, the correct answer was {}".format(correct_answer))
+        break
+
+else:
+    print("Well done, you reached {}".format(next_number))
