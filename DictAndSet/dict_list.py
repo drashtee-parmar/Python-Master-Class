eb0ac473caa2e467b0e06314e01f50d0cca94a7e
Drashtee Parmar <drashteeparmar@gmail.com>
2025-02-14 17:36:04 -0500

    dict computer list

--- DictAndSet/dict_list.py
+++ DictAndSet/dict_list.py
@@ -7,12 +7,32 @@ available_parts = {"1": "computer",
                    }
 
 current_choice = None
+computer_parts = []  # create an empty list
+
+# while current_choice != "0":
+#     if current_choice in available_parts:
+#         chosen_part = available_parts[current_choice]
+#         print(f"Adding {chosen_part}")
+#     else:
+#         print("Please add options from the list")
+#         for key, value in available_parts.items():
+#             print(f"{key}: {value}")
+#         print("0: to finish")
+
+
 while current_choice != "0":
     if current_choice in available_parts:
         chosen_part = available_parts[current_choice]
-        print(f"Adding {chosen_part}")
+        if chosen_part in computer_parts:
+            # it's already in, so remove it
+            print(f"Removing {chosen_part}")
+            computer_parts.remove(chosen_part)
+        else:
+            print(f"Adding {chosen_part}")
+            computer_parts.append(chosen_part)
+        print(f"Your list now contains: {computer_parts}")
     else:
-        print("Please add options from the list")
+        print("Please add options from the list:")
         for key, value in available_parts.items():
             print(f"{key}: {value}")
         print("0: to finish")
